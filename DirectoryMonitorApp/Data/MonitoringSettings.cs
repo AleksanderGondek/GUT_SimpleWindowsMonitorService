69b7b6afde62afba547482883d1a9a36af93c22c
Aleksander Gondek <gondekaleksander@hotmail.com>
2014-12-14 23:13:43 +0100

    Wcf client added

---
+++ DirectoryMonitorApp/Data/MonitoringSettings.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using DirectoryMonitorApp.DirectoryMonitorServiceClient;
+
+namespace DirectoryMonitorApp.Data
+{
+    internal class MonitoringSettings
+    {
+        private DmsClient _dmsClient;
+
+        private string _directoryToWatch;
+        private string[] _changesToWatch;
+        private string _fileTypeToWatch;
+        private bool _shouldWatchSubDirs;
+
+        public bool Status { get; private set; }
+
+        public void GetData()
+        {
+            if (_dmsClient == null)
+            {
+                _dmsClient = new DmsClient();
+            }
+
+            Status = @"OK".Equals(_dmsClient.GetStatus());
+            _directoryToWatch = _dmsClient.GetDirectoryToWatch();
+            _changesToWatch = _dmsClient.GetChangesToWatch().Split(',');
+            _fileTypeToWatch = _dmsClient.GetFiletypeToWatch();
+            _shouldWatchSubDirs = Convert.ToBoolean(_dmsClient.GetShouldWatchSubdirectories());
+        }
+
+
+        public string DirectoryToWatch
+        {
+            get { return _directoryToWatch; }
+            set
+            {
+                if (_dmsClient.SetDirectoryToWatch(value))
+                {
+                    _directoryToWatch = value;
+                }
+            }
+        }
+
+        public string[] ChangesToWatch
+        {
+            get { return _changesToWatch; }
+            set
+            {
+                if (_dmsClient.SetChangesToWatch(string.Join(",",value)))
+                {
+                    _changesToWatch = value;
+                }
+            }
+        }
+
+        public string FileTypeToWatch
+        {
+            get { return _fileTypeToWatch; }
+            set
+            {
+                if (_dmsClient.SetFiletypeToWatch(value))
+                {
+                    _fileTypeToWatch = value;
+                }
+            }
+        }
+
+        public bool ShouldWatchSubDirs
+        {
+            get { return _shouldWatchSubDirs; }
+            set
+            {
+                if (_dmsClient.SetShouldWatchSubdirectories(Convert.ToString(value)))
+                {
+                    _shouldWatchSubDirs = value;
+                }
+            }
+        }
+    }
+}
---
+++ DirectoryMonitorApp/DirectoryMonitorServiceClient/DmsClient.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using DirectoryMonitor.Communication;
+
+namespace DirectoryMonitorApp.DirectoryMonitorServiceClient
+{
+    internal class DmsClient : IDisposable
+    {
+        private IDirectoryMonitorCommands _directoryMonitorChannel;
+        public bool Connect()
+        {
+            try
+            {
+                var config = new ChannelFactory<IDirectoryMonitorCommands>(new WebHttpBinding(), "http://localhost:8000");
+                config.Endpoint.Behaviors.Add(new WebHttpBehavior());
+                _directoryMonitorChannel = config.CreateChannel();
+
+                return @"OK".Equals(_directoryMonitorChannel.GetStatus());
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public string GetStatus()
+        {
+            return _directoryMonitorChannel.GetStatus();
+        }
+        public string GetDirectoryToWatch()
+        {
+            return _directoryMonitorChannel.GetDirectoryToWatch();
+        }
+        public string GetChangesToWatch()
+        {
+            return _directoryMonitorChannel.GetChangesToWatch();
+        }
+        public string GetFiletypeToWatch()
+        {
+            return _directoryMonitorChannel.GetFiletypeToWatch();
+        }
+        public string GetShouldWatchSubdirectories()
+        {
+            return _directoryMonitorChannel.GetShouldWatchSubdirectories();
+        }
+        public bool SetDirectoryToWatch(string directory)
+        {
+            return _directoryMonitorChannel.SetDirectoryToWatch(directory);
+        }
+        public bool SetChangesToWatch(string changes)
+        {
+            return _directoryMonitorChannel.SetChangesToWatch(changes);
+        }
+        public bool SetFiletypeToWatch(string filetype)
+        {
+            return _directoryMonitorChannel.SetFiletypeToWatch(filetype);
+        }
+        public bool SetShouldWatchSubdirectories(string watchSubdirs)
+        {
+            return _directoryMonitorChannel.SetShouldWatchSubdirectories(watchSubdirs);
+        }
+
+        public void Dispose()
+        {
+            _directoryMonitorChannel = null;
+        }
+    }
+}
